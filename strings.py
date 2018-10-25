03aec1607cce0833643af1e2befffd6cb6340c98
gsanchezm <gilsanchezts@outlook.com>
2018-10-25 12:01:54 -0500

    modified strings.py: added slices examples

--- strings.py
+++ strings.py
@@ -4,3 +4,17 @@ fer.upper()
 fer.capitalize()
 fer.lower()
 fer.casefold()
+
+#Slices examples
+word = 'ferrocarril'
+word1 = word [1:4]
+word2 = word [1:8]
+word3 = word [::-1]
+word4 = word [:8:3] #should print fra
+word5 = word [::2]
+print(word)  #should print ferrocarril
+print(word1) #should print err
+print(word2) #should print errocar
+print(word3) #should print ferrocarril mirrowed
+print(word4) #should print fra
+print(word5) #should print froarl
