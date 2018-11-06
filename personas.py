d09754c4f07f22b65bdd62ddda71d3e6c317a602
Fer Diaz <ferb.diaz@gmail.com>
2018-11-05 20:25:30 -0600

    Added persons file for OOP practice

---
+++ personas.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+        def say_hello(self):
+            print('Hola, mi nombre es {} y tengo {} años'.format(self.name, self.age))
+
+if __name__ == '__main__':
+    person = Person('Fer', 28)
+
+    print('Edad {}'.format(person.age))
+    person.say_hello()
